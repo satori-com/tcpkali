784b036862aa44b09fdfe9285740527f178ebd38
lwalkin <lwalkin@machinezone.com>
2014-09-22 17:32:03 -0500

    atomic64

--- tcpkali_atomic.h
+++ tcpkali_atomic.h
@@ -2,6 +2,7 @@
 #define TCPKALI_ATOMIC_H
 
 typedef uint32_t atomic_t;
+typedef uint64_t atomic64_t;
 
 static inline void __attribute__((unused)) atomic_increment(atomic_t *i) {
     asm volatile("lock incl %0" : "+m" (*i));
@@ -9,6 +10,10 @@ static inline void __attribute__((unused)) atomic_increment(atomic_t *i) {
 
 static inline void __attribute__((unused)) atomic_decrement(atomic_t *i) {
     asm volatile("lock decl %0" : "+m" (*i));
+}
+
+static inline void __attribute__((unused)) atomic_add(atomic64_t *i, int v) {
+    asm volatile("lock addq %1, %0" : "+m" (*i) : "ir" (v));
 }
 
 #endif  /* TCPKALI_ATOMIC_H */
