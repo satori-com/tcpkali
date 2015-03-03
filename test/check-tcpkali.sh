698024525c417cf902284347dc2debb98f19495d
lwalkin <lwalkin@machinezone.com>
2015-03-02 20:02:36 -0800

    fixed tests

--- test/check-tcpkali.sh
+++ test/check-tcpkali.sh
@@ -8,21 +8,22 @@ if [ -z "${TCPKALI}" ]; then
 fi
 
 check() {
-    if [ -z "$testno" ]; then testno=0; fi
-    testno=$(($testno+1))
+    local testno
+    testno=$1
+    shift
     echo "Test $testno: $@" >&2
     $@
 }
 
-check ${TCPKALI} --connections=20 --duration=1 -l1271 127.1:1271
-check ${TCPKALI} -c10 -T1 --message Z --message-rate=1 -l1271 127.1:1271
-check ${TCPKALI} -c10 -T1 -m Z --channel-bandwidth=10kbps -l1271 127.1:1271
-check ${TCPKALI} --connections=10 --duration=1 -m Z -l1271 127.1:1271
+check 1  ${TCPKALI} --connections=20 --duration=1 -l1271 127.1:1271
+check 2  ${TCPKALI} -c10 -T1 --message Z --message-rate=1 -l1271 127.1:1271
+check 3  ${TCPKALI} -c10 -T1 -m Z --channel-bandwidth=10kbps -l1271 127.1:1271
+check 4  ${TCPKALI} --connections=10 --duration=1 -m Z -l1271 127.1:1271
 
-check ${TCPKALI} -T1s --ws -l1271 127.1:1271 | egrep -q "Total data sent:\s+149 bytes"
-check ${TCPKALI} -T1s --ws -l1271 127.1:1271 | egrep -q "Total data received:\s+278 bytes"
-check ${TCPKALI} -T1s --ws -l1271 127.1:1271 --first-message ABC | egrep -q "Total data sent:\s+154 bytes"
-check ${TCPKALI} -T1s --ws -l1271 127.1:1271 --first-message ABC | egrep -q "Total data received:\s+283 bytes"
+check 4  ${TCPKALI} -T1s --ws -l1271 127.1:1271 | egrep -q "Total data sent:\s+149 bytes"
+check 6  ${TCPKALI} -T1s --ws -l1271 127.1:1271 | egrep -q "Total data received:\s+278 bytes"
+check 7  ${TCPKALI} -T1s --ws -l1271 127.1:1271 --first-message ABC | egrep -q "Total data sent:\s+158 bytes"
+check 8  ${TCPKALI} -T1s --ws -l1271 127.1:1271 --first-message ABC | egrep -q "Total data received:\s+287 bytes"
 
-check ${TCPKALI} -T1s --ws -l1271 127.1:1271 --message ABC
-check ${TCPKALI} -T1s --ws -l1271 127.1:1271 --first-message ABC --message foo
+check 9  ${TCPKALI} -T1s --ws -l1271 127.1:1271 --message ABC
+check 10 ${TCPKALI} -T1s --ws -l1271 127.1:1271 --first-message ABC --message foo
