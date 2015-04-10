b4619d6d4ee4323d0e4da89eeed583238ea3c928
lwalkin <lwalkin@machinezone.com>
2015-04-09 17:54:24 -0700

    avoid warning about unexposed strcasestr

--- src/tcpkali_terminfo.c
+++ src/tcpkali_terminfo.c
@@ -24,6 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+#define _GNU_SOURCE /* to expose strcasestr(3) */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
