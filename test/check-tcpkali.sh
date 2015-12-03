838462db4a263698f795a701b32acd2b1157b7dc
Lev Walkin <lwalkin@machinezone.com>
2015-12-03 15:42:18 -0800

    test which has to pass on travis

--- test/check-tcpkali.sh
+++ test/check-tcpkali.sh
@@ -7,26 +7,27 @@ if [ -z "${TCPKALI}" ]; then
     TCPKALI=../src/tcpkali
 fi
 
-PORT=1234
+PORT=1230
 
 check() {
-    local testno
-    testno=$1
-    shift
-    echo "Test $testno: $*" >&2
+    local testno=$1
+    local togrep=$2
+    shift 2
     PORT=$(($PORT+1))
-    $@ -T1 --source-ip 127.1 -l${PORT} 127.1:${PORT}
+    local rest_opts="-T1 --source-ip 127.1 -l${PORT} 127.1:${PORT}"
+    echo "Test $testno: $* ${rest_opts}" >&2
+    $@ ${rest_opts} | egrep "$togrep"
 }
 
-check 1  ${TCPKALI} --connections=20 --duration=1
-check 2  ${TCPKALI} -c10 --message Z --message-rate=1
-check 3  ${TCPKALI} -c10 -m Z --channel-bandwidth-upstream=10kbps
-check 4  ${TCPKALI} --connections=10 --duration=1 -m Z
+check 1 "." ${TCPKALI} --connections=20 --duration=1
+check 2 "." ${TCPKALI} -c10 --message Z --message-rate=1
+check 3 "." ${TCPKALI} -c10 -m Z --channel-bandwidth-upstream=10kbps
+check 4 "." ${TCPKALI} --connections=10 --duration=1 -m Z
 
-check 5  ${TCPKALI} --ws | egrep -q "Total data sent:[ ]+149 bytes"
-check 6  ${TCPKALI} --ws | egrep -q "Total data received:[ ]+278 bytes"
-check 7  ${TCPKALI} --ws | egrep -q "Total data sent:[ ]+158 bytes"
-check 8  ${TCPKALI} --ws | egrep -q "Total data received:[ ]+287 bytes"
+check 5 "Total data sent:[ ]+149 bytes"     ${TCPKALI} --ws
+check 6 "Total data received:[ ]+278 bytes" ${TCPKALI} --ws
+check 7 "Total data sent:[ ]+158 bytes"     ${TCPKALI} --ws --first-message ABC
+check 8 "Total data received:[ ]+287 bytes" ${TCPKALI} --ws --first-message ABC
 
-check 9  ${TCPKALI} --ws --message ABC
-check 10 ${TCPKALI} --ws --first-message ABC --message foo
+check 9 "." ${TCPKALI} --ws --message ABC
+check 10 "." ${TCPKALI} --ws --first-message ABC --message foo
