bc4af151fa7b476e61e037a999f041b92e567661
lwalkin <lwalkin@machinezone.com>
2016-09-15 20:22:24 +0000

    recognize -T1y for years

--- src/tcpkali.c
+++ src/tcpkali.c
@@ -152,7 +152,8 @@ static struct multiplier s_multiplier[] = {
     { "s", 1 }, { "second", 1 }, { "seconds", 1 },
     { "m", 60 }, { "min", 60 }, { "minute", 60 }, { "minutes", 60 },
     { "h", 3600 }, { "hr", 3600 }, { "hour", 3600 }, { "hours", 3600 },
-    { "d", 86400 }, { "day", 86400 }, { "days", 86400 }
+    { "d", 86400 }, { "day", 86400 }, { "days", 86400 },
+    { "y", 31536000 }, { "year", 31536000 }, { "years", 31536000 }
 };
 static struct multiplier bw_multiplier[] = {
     /* bits per second */
